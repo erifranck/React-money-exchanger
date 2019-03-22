1f70a2e6e97e06434465961a1f59406900c3e123
erifranck <erifranjose@hotmail.com>
2019-03-22 12:01:38 -0500

    create store with redux devtools

--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -1,11 +1,16 @@
 import {createStore as createReduxStore, applyMiddleware} from "redux";
 import {rootReducer} from "store/reducers";
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import {rootSaga} from "store/sagas";
+
 
 export function createStore() {
     const sagaMiddleware = createSagaMiddleware()
-    const store = createReduxStore(rootReducer, applyMiddleware(sagaMiddleware));
+    const compose = composeWithDevTools(
+        applyMiddleware(sagaMiddleware)
+    );
+    const store = createReduxStore(rootReducer, compose);
     sagaMiddleware.run(rootSaga);
     return store;
 }
--- src/store/reducers/exchange.ts
+++ src/store/reducers/exchange.ts
@@ -2,36 +2,61 @@ import {createReducer, ActionCreators, createActions, CreatedActions} from 'redu
 
 export const enum Type {
     EXCHANGE = 'EXCHANGE',
+    START_FETCH = 'START_FETCH',
+    GET_EXCHANGE_VALUE = 'GET_EXCHANGE_VALUE',
+    GET_EXCHANGE_VALUE_SUCCESS = 'GET_EXCHANGE_VALUE_SUCCESS',
+    GET_EXCHANGE_VALUE_FAIL = 'GET_EXCHANGE_VALUE_FAIL',
 }
 
 interface Creator extends ActionCreators {
-    appStart: () => { type: Type.EXCHANGE };
+    exchange: () => { type: Type.EXCHANGE; usd: string };
+    startFetch: () => { type: Type.START_FETCH };
+    getExchangeValue: () => { type: Type.GET_EXCHANGE_VALUE };
+    getExchangeValueSuccess: (conversion: string) => { type: Type.GET_EXCHANGE_VALUE_SUCCESS, conversionValue: string };
+    getExchangeValueFail: (error: any) => { type: Type.GET_EXCHANGE_VALUE_FAIL, error: any };
 }
-interface State {
 
-}
-
-const {Types, Creators}: CreatedActions = createActions({
+const {Creators}: CreatedActions = createActions({
     exchange: ['usd'],
     getExchangeValue: null,
+    startFetch: null,
     getExchangeValueSuccess: ['conversionValue'],
     getExchangeValueFail: ['error'],
 });
 
 export const actions = Creators as Creator;
-export const types = Types;
 
-const INITIAL_STATE = {
-    'usd': 0,
-    'eur': 0,
-    'conversionValue': 0
+export interface ExchangeState {
+    readonly usd: number;
+    readonly eur: number;
+    readonly fetch: boolean;
+    readonly conversionValue: number;
+}
+
+const INITIAL_STATE: ExchangeState = {
+    usd: 0,
+    eur: 0,
+    conversionValue: 0,
+    fetch: false
 };
 
-const getExchangeSuccess = (state, action) => ({
+const getExchangeSuccess = (state: ExchangeState, action): ExchangeState => ({
     ...state,
+    fetch: false,
     'conversionValue': action.conversionValue,
 });
 
+const onFetch = (state: ExchangeState, action): ExchangeState => ({
+    ...state,
+    fetch: true,
+});
+
+const exchangeEUR = (state: ExchangeState, action): ExchangeState => ({
+    ...state,
+    eur: action.usd / state.conversionValue,
+});
 export default createReducer(INITIAL_STATE, {
-    [Type.EXCHANGE]: getExchangeSuccess,
+    [Type.START_FETCH]: onFetch,
+    [Type.GET_EXCHANGE_VALUE_SUCCESS]: getExchangeSuccess,
+    [Type.EXCHANGE]: exchangeEUR,
 });
