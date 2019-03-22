bd7db0c4cb1eb8e10b5496802ba2df3848169844
erifranck <erifranjose@hotmail.com>
2019-03-22 12:02:05 -0500

    request exchange value

--- src/layouts/App/App.tsx
+++ src/layouts/App/App.tsx
@@ -6,15 +6,24 @@ import {Header} from "components/Header/Header";
 import {Footer} from "components/Footer/Footer";
 import {Exchanger} from "components/Exchanger/Exchanger";
 import {createStore} from "store/createStore";
+import {actions} from "store/reducers/exchange";
 
-export const App = () => (
-    <Provider store={createStore()}>
-       <ThemeProvider theme={theme}>
-           <Fragment>
-               <Header />
-               <Exchanger />
-               <Footer />
-           </Fragment>
-       </ThemeProvider>
-    </Provider>
-)
+const store = createStore();
+export class App extends React.Component {
+    componentDidMount() {
+        store.dispatch(actions.getExchangeValue());
+    }
+    render () {
+        return (
+            <Provider store={store}>
+                <ThemeProvider theme={theme}>
+                    <Fragment>
+                        <Header />
+                        <Exchanger />
+                        <Footer />
+                    </Fragment>
+                </ThemeProvider>
+            </Provider>
+        )
+    }
+}
